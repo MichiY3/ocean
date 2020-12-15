b05c81f4d4a485b76ef7eb84064568d1d0fb951f
MichiY3 <michiyoy@hotmail.com>
2020-12-15 20:01:14 +0900

    PCサイズモック 完成

--- js2/sample.js
+++ js2/sample.js
@@ -43,4 +43,13 @@ var mySwiper = new Swiper ('.swiper-container', {
 });
 
 
+var mySwiper = new Swiper ('.swiper-container', {
+  loop: true,
+  slidesPerView: 2,
+    spaceBetween: 10,
+    centeredSlides : true,
+  pagination: '.swiper-pagination',
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev'
+});
 
